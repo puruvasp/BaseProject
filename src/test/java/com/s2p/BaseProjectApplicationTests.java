0ec5bc53461d8f1f186b9e407e323c4870da6b8f
puruv <puruvas0954@gmail.com>
2025-12-22 16:15:45 +0530

    I am Batman

---
+++ src/test/java/com/s2p/BaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
